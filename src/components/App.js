3b1cf7b173836440c8dbdcc9ae56867c7d3926d2
Sergey <sergey.riedel@gmail.com>
2019-07-28 12:23:01 +0200

    Fix classnames

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ const App = () => {
   return (
     <div className="app">
       <div className="ui container">
-        <h1 class="ui left floated header" style={{ paddingTop: '20px' }}>
+        <h1 className="ui left floated header" style={{ paddingTop: '20px' }}>
           Calorik
         </h1>
 
-        <div class="ui clearing divider" />
-        <div class="ui three column doubling stackable grid">
-          <div class="column">
-            <h3 class="ui header">Personal data</h3>
+        <div className="ui clearing divider" />
+        <div className="ui three column doubling stackable grid">
+          <div className="column">
+            <h3 className="ui header">Personal data</h3>
             <form className="ui large form">
               <div className="six wide field">
                 <label>Age:</label>
@@ -20,23 +20,23 @@ const App = () => {
 
               <div className="inline fields">
                 <label for="fruit">Gender:</label>
-                <div class="field">
-                  <div class="ui radio checkbox">
+                <div className="field">
+                  <div className="ui radio checkbox">
                     <input
                       type="radio"
                       name="gender"
                       checked
                       tabindex="0"
-                      class="hidden"
+                      className="hidden"
                     />
                     <label>Male</label>
                   </div>
-                  <div class="ui radio checkbox">
+                  <div className="ui radio checkbox">
                     <input
                       type="radio"
                       name="gender"
                       tabindex="0"
-                      class="hidden"
+                      className="hidden"
                     />
                     <label>Female</label>
                   </div>
@@ -53,55 +53,55 @@ const App = () => {
             </form>
           </div>
 
-          <div class="column">
-            <h3 class="ui header">Activity level</h3>
+          <div className="column">
+            <h3 className="ui header">Activity level</h3>
             <form className="ui large form">
               <div className="grouped fields">
                 <label>Select your activity level:</label>
-                <div class="field">
-                  <div class="ui radio checkbox">
+                <div className="field">
+                  <div className="ui radio checkbox">
                     <input
                       type="radio"
                       name="activity"
                       checked=""
                       tabindex="0"
-                      class="hidden"
+                      className="hidden"
                     />
                     <label>Little / No exercise</label>
                   </div>
                 </div>
-                <div class="field">
-                  <div class="ui radio checkbox">
+                <div className="field">
+                  <div className="ui radio checkbox">
                     <input
                       type="radio"
                       name="fruit"
                       checked=""
                       tabindex="0"
-                      class="hidden"
+                      className="hidden"
                     />
                     <label>3 times a week</label>
                   </div>
                 </div>
-                <div class="field">
-                  <div class="ui radio checkbox">
+                <div className="field">
+                  <div className="ui radio checkbox">
                     <input
                       type="radio"
                       name="fruit"
                       checked=""
                       tabindex="0"
-                      class="hidden"
+                      className="hidden"
                     />
                     <label>4 times a week</label>
                   </div>
                 </div>
-                <div class="field">
-                  <div class="ui radio checkbox">
+                <div className="field">
+                  <div className="ui radio checkbox">
                     <input
                       type="radio"
                       name="fruit"
                       checked=""
                       tabindex="0"
-                      class="hidden"
+                      className="hidden"
                     />
                     <label>5 times a week</label>
                   </div>
@@ -110,45 +110,45 @@ const App = () => {
             </form>
           </div>
 
-          <div class="column">
-            <h3 class="ui header">Your goal</h3>
+          <div className="column">
+            <h3 className="ui header">Your goal</h3>
             <form className="ui large form">
               <div className="grouped fields">
                 <label>Select your goal:</label>
-                <div class="field">
-                  <div class="ui radio checkbox">
+                <div className="field">
+                  <div className="ui radio checkbox">
                     <input
                       type="radio"
                       name="goal"
                       checked=""
                       tabindex="0"
-                      class="hidden"
+                      className="hidden"
                     />
                     <label>Cut</label>
                   </div>
                 </div>
 
-                <div class="field">
-                  <div class="ui radio checkbox">
+                <div className="field">
+                  <div className="ui radio checkbox">
                     <input
                       type="radio"
                       name="goal"
                       checked=""
                       tabindex="0"
-                      class="hidden"
+                      className="hidden"
                     />
                     <label>Maintain</label>
                   </div>
                 </div>
 
-                <div class="field">
-                  <div class="ui radio checkbox">
+                <div className="field">
+                  <div className="ui radio checkbox">
                     <input
                       type="radio"
                       name="goal"
                       checked=""
                       tabindex="0"
-                      class="hidden"
+                      className="hidden"
                     />
                     <label>Gain</label>
                   </div>
@@ -158,11 +158,14 @@ const App = () => {
           </div>
         </div>
 
-        <div class="ui horizontal divider">
+        <div className="ui horizontal divider">
           <button className="ui huge secondary button">Calculate</button>
         </div>
 
-        <h1 class="ui header" style={{ fontSize: '50px', marginTop: '70px' }}>
+        <h1
+          className="ui header"
+          style={{ fontSize: '50px', marginTop: '70px' }}
+        >
           Your calories: 0
         </h1>
       </div>
