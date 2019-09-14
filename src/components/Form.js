d0218fc263ee228061f596d648f1cbd168f0761a
Sergey <sergey.riedel@gmail.com>
2019-09-14 15:05:08 +0200

    Get values from inputs to state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,149 +1,193 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Form extends Component {
-  state = {};
+const Form = () => {
+  const [data, setData] = useState({
+    age: 30,
+    gender: 'male',
+    weight: 85,
+    height: 180,
+    activityLevel: '0',
+    goal: 'cut'
+  });
 
-  render() {
-    return (
-      <form className="ui large form">
-        <div className="ui three column doubling stackable grid">
-          <div className="column">
-            <h3 className="ui header">Personal data</h3>
+  const handleChange = e =>
+    setData({ ...data, [e.target.name]: e.target.value });
 
-            <div className="six wide field">
-              <label>Age:</label>
-              <input type="text" name="age" />
-            </div>
+  return (
+    <form className="ui large form">
+      <div className="ui three column doubling stackable grid">
+        <div className="column">
+          <h3 className="ui header">Personal data</h3>
 
-            <div className="inline fields">
-              <label for="fruit">Gender:</label>
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    name="gender"
-                    value="male"
-                    checked
-                    tabindex="0"
-                  />
-                  <label>Male</label>
-                </div>
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    name="gender"
-                    value="female"
-                    tabindex="0"
-                  />
-                  <label>Female</label>
-                </div>
-              </div>
-            </div>
-            <div className="six wide field">
-              <label>Weight:</label>
-              <input type="text" name="weight" />
-            </div>
-            <div className="six wide field">
-              <label>Height:</label>
-              <input type="text" name="height" />
-            </div>
+          <div className="six wide field">
+            <label>Age:</label>
+            <input
+              type="text"
+              name="age"
+              value={data.age}
+              onChange={handleChange}
+            />
           </div>
 
-          <div className="column">
-            <h3 className="ui header">Activity level</h3>
-
-            <div className="grouped fields">
-              <label>Select your activity level:</label>
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    name="activity"
-                    value="no-exercise"
-                    tabindex="0"
-                    checked
-                  />
-                  <label>Little / No exercise</label>
-                </div>
+          <div className="inline fields">
+            <label htmlFor="gender">Gender:</label>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  checked={data.gender === 'male'}
+                  tabindex="0"
+                  onChange={handleChange}
+                />
+                <label>Male</label>
               </div>
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    name="activity"
-                    value="3-times"
-                    tabindex="0"
-                  />
-                  <label>3 times a week</label>
-                </div>
-              </div>
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    name="activity"
-                    value="4-times"
-                    tabindex="0"
-                  />
-                  <label>4 times a week</label>
-                </div>
-              </div>
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    name="activity"
-                    value="5-times"
-                    tabindex="0"
-                  />
-                  <label>5 times a week</label>
-                </div>
+              <div className="ui radio checkbox">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  checked={data.gender === 'female'}
+                  tabindex="0"
+                  onChange={handleChange}
+                />
+                <label>Female</label>
               </div>
             </div>
           </div>
+          <div className="six wide field">
+            <label>Weight:</label>
+            <input
+              type="text"
+              name="weight"
+              value={data.weight}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="six wide field">
+            <label>Height:</label>
+            <input
+              type="text"
+              name="height"
+              value={data.height}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
 
-          <div className="column">
-            <h3 className="ui header">Your goal</h3>
+        <div className="column">
+          <h3 className="ui header">Activity level</h3>
 
-            <div className="grouped fields">
-              <label>Select your goal:</label>
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    name="goal"
-                    value="cut"
-                    tabindex="0"
-                    checked
-                  />
-                  <label>Cut</label>
-                </div>
+          <div className="grouped fields">
+            <label>Select your activity level:</label>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input
+                  type="radio"
+                  name="activityLevel"
+                  value="0"
+                  tabindex="0"
+                  checked={data.activityLevel === '0'}
+                  onChange={handleChange}
+                />
+                <label>Little / No exercise</label>
               </div>
-
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    name="goal"
-                    value="maintain"
-                    tabindex="0"
-                  />
-                  <label>Maintain</label>
-                </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input
+                  type="radio"
+                  name="activityLevel"
+                  value="3"
+                  checked={data.activityLevel === '3'}
+                  tabindex="0"
+                  onChange={handleChange}
+                />
+                <label>3 times a week</label>
               </div>
-
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input type="radio" name="goal" value="gain" tabindex="0" />
-                  <label>Gain</label>
-                </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input
+                  type="radio"
+                  name="activityLevel"
+                  value="4"
+                  checked={data.activityLevel === '4'}
+                  tabindex="0"
+                  onChange={handleChange}
+                />
+                <label>4 times a week</label>
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input
+                  type="radio"
+                  name="activityLevel"
+                  value="5"
+                  checked={data.activityLevel === '5'}
+                  tabindex="0"
+                  onChange={handleChange}
+                />
+                <label>5 times a week</label>
               </div>
             </div>
           </div>
         </div>
-      </form>
-    );
-  }
-}
+
+        <div className="column">
+          <h3 className="ui header">Your goal</h3>
+
+          <div className="grouped fields">
+            <label>Select your goal:</label>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input
+                  type="radio"
+                  name="goal"
+                  value="cut"
+                  checked={data.goal === 'cut'}
+                  tabindex="0"
+                  onChange={handleChange}
+                />
+                <label>Cut</label>
+              </div>
+            </div>
+
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input
+                  type="radio"
+                  name="goal"
+                  value="maintain"
+                  checked={data.goal === 'maintain'}
+                  tabindex="0"
+                  onChange={handleChange}
+                />
+                <label>Maintain</label>
+              </div>
+            </div>
+
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input
+                  type="radio"
+                  name="goal"
+                  value="gain"
+                  checked={data.goal === 'gain'}
+                  tabindex="0"
+                  onChange={handleChange}
+                />
+                <label>Gain</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {JSON.stringify(data)}
+    </form>
+  );
+};
 
 export default Form;
