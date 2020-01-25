e461c36ee94d7812c3ce5c42bb659f1736a4116f
Sergey <sergey.riedel@gmail.com>
2020-01-25 12:23:21 +0100

    Add semantic-ui-react to Result

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Table, Header } from "semantic-ui-react";
+import PropTypes from "prop-types";
 
-import { calculateCalories } from './../helpers';
+import { calculateCalories } from "./../helpers";
 
 const Result = ({ data }) => {
   const { toCut, toGain, toMaintain } = calculateCalories(data);
 
   return (
-    <div style={{ paddingTop: '30px' }}>
-      <table className="ui large celled fixed table">
-        <thead>
-          <tr>
-            <th>Cut</th>
-            <th>Maintain</th>
-            <th>Gain</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <h2>{toCut ? toCut : 0} kcal</h2>
-            </td>
-            <td>
-              <h2>{toMaintain ? toMaintain : 0} kcal</h2>
-            </td>
-            <td>
-              <h2>{toGain ? toGain : 0} kcal</h2>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div style={{ paddingTop: "30px" }}>
+      <Table celled fixed>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Cut</Table.HeaderCell>
+            <Table.HeaderCell>Maintain</Table.HeaderCell>
+            <Table.HeaderCell>Gain</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>
+              <Header as="h2">{toCut ? toCut : 0} kcal</Header>
+            </Table.Cell>
+            <Table.Cell>
+              <Header as="h2">{toMaintain ? toMaintain : 0} kcal</Header>
+            </Table.Cell>
+            <Table.Cell>
+              <Header as="h2">{toGain ? toGain : 0} kcal</Header>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
     </div>
   );
 };
@@ -37,18 +38,18 @@ const Result = ({ data }) => {
 Result.propTypes = {
   data: PropTypes.shape({
     age: PropTypes.number,
-    gender: PropTypes.oneOf(['male', 'female']),
+    gender: PropTypes.oneOf(["male", "female"]),
     weight: PropTypes.number,
     height: PropTypes.number,
-    formula: PropTypes.oneOf(['harris-benedict', 'mifflin-st-jeor']),
+    formula: PropTypes.oneOf(["harris-benedict", "mifflin-st-jeor"]),
     activityLevel: PropTypes.oneOf([
-      'no-exercise',
-      'light',
-      'moderate',
-      'heavy',
-      'very-heavy'
+      "no-exercise",
+      "light",
+      "moderate",
+      "heavy",
+      "very-heavy"
     ]),
-    goal: PropTypes.oneOf(['cut', 'maintain', 'gain'])
+    goal: PropTypes.oneOf(["cut", "maintain", "gain"])
   })
 };
 
