c84a3a87be8f9547980d233546d63ea153c1d22e
Sergey <sergey.riedel@gmail.com>
2020-07-01 14:00:54 +0200

    Animate calories number output

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,11 +1,17 @@
 import React, { useEffect } from "react";
 import { Header } from "semantic-ui-react";
 import PropTypes from "prop-types";
+import { useSpring, animated } from "react-spring";
 
 import { calculateCalories } from "./../helpers";
 
 const Result = ({ data }) => {
   const resultCalories = calculateCalories(data);
+
+  const animation = useSpring({
+    from: { value: 0 },
+    to: { value: isNaN(resultCalories) ? 0 : resultCalories },
+  });
 
   useEffect(() => {
     const title = "Calorik";
@@ -21,7 +27,11 @@ const Result = ({ data }) => {
     resultCalories > 0 && (
       <div style={{ textAlign: "center", paddingTop: "30px" }}>
         <Header as="h1">
-          You will need {resultCalories} kcal to {data.goal}
+          You will need{" "}
+          <animated.span>
+            {animation.value.interpolate((val) => Math.floor(val))}
+          </animated.span>{" "}
+          kcal to {data.goal}
         </Header>
       </div>
     )
