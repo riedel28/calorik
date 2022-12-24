c5d64c21f675cabcdf69dc56af978d604dcbce48
Sergey Riedel <sergey.riedel@gmail.com>
2022-12-24 15:52:19 +0100

    Define server config and aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      context: path.resolve(__dirname, 'src/context'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      helpers: path.resolve(__dirname, 'src/helpers'),
+    },
+  },
 });
