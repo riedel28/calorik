fc633925ea1eddc9823951c79db8ae82ff6dd5fe
Sergey Riedel <sergey.riedel@gmail.com>
2023-12-25 10:24:51 +0100

    fix: redirect from '/' to '/en'

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
+import { NextResponse } from 'next/server';
 import { match } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
+import type { NextRequest } from 'next/server';
 
 let headers = { 'accept-language': 'en-US,en;q=0.5' };
 let languages = new Negotiator({ headers }).languages();
@@ -7,3 +9,10 @@ let locales = ['en-US', 'de-DE', 'ru-RU'];
 let defaultLocale = 'en-US';
 
 match(languages, locales, defaultLocale); // -> 'en-US'
+
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  if (url.pathname === '/') {
+    return NextResponse.rewrite(new URL('/en', request.url));
+  }
+}
